d641a7f5712ace8cec972a4a79856b5b9be860e3
gobaru <gobaru.1993@gmail.com>
2021-03-15 22:55:35 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'rooms/new'
+  get 'rooms/create'
   get 'users/edit'
   devise_for :users
   get 'messages/index'
   root to: "messages#index"
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 end
